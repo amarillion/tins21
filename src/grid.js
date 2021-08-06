a54a0126b5e9a105900948d026785b53b276c276
Martijn van Iersel <martijni@topdesk.com>
2021-08-06 21:35:20 +0200

    Separate grid from polygons

---
+++ src/grid.js
@@ -0,0 +1,18 @@
+
+class Cell {
+	constructor(area) {
+		this.area = area;
+			// sides
+
+	}
+}
+
+class Grid {
+	constructor() {
+
+	}
+
+	getAdjacent(cell) {
+
+	}
+}
--- src/scenes/Game.js
+++ src/scenes/Game.js
@@ -3,6 +3,40 @@ import Phaser from 'phaser';
 import Mushroom from '../sprites/Mushroom.js';
 import { getCairoTesselation, getDiamondTesselation, getHexagonalTesselation, getSquareTesselation, getTriangleTesselation } from '../tesselate.js';
 
+const SCREENH = 600;
+const SCREENW = 800;
+const SCALE = 64;
+
+function initGrid(tesselation) {
+	const grid = [];
+
+	const { offset, primitiveUnit } = tesselation;
+
+	const createPrimitiveUnit = (xco, yco) => {
+		let i = 0;
+		const elements = [];
+		for (const element of primitiveUnit) {
+			elements.push({ xco, yco, element });
+		}
+		return elements;
+	}
+
+	let xco = 0;
+	let yco = 0;
+	while (yco < SCREENH) {
+		let secondX = xco;
+		let secondY = yco;
+		while (secondX < SCREENW) {
+			const elements = createPrimitiveUnit(secondX, secondY);
+			grid.push(elements);
+			secondX += offset[2] * SCALE;
+		}
+		yco += offset[1] * SCALE;
+	}
+
+	return grid;
+}
+
 export default class extends Phaser.Scene {
 	constructor () {
 		super({ key: 'GameScene' });
@@ -10,31 +44,26 @@ export default class extends Phaser.Scene {
 	init () {}
 	preload () {}
 
-	renderTesselation(tesselation) {
-		const SCREENH = 600;
-		const SCREENW = 800;
+	renderPolygons(grid, tesselation) {
 
-		const { shape, offset, primitiveUnit } = tesselation;
-
+		const { shape } = tesselation;
 		const scaledShape = [];
 
-		const SCALE = 64;
 		for (let i = 0; i < shape.length; ++i) {
 			scaledShape[i] = shape[i] * SCALE;
 		}
 
-		const createPrimitiveUnit = (xco, yco) => {
-			let i = 0;
-			for (const element of primitiveUnit) {
+		for (const cell of grid) {
+			for (const { xco, yco, element } of cell) {
 				const poly = new Phaser.GameObjects.Polygon(
 					this, 
 					xco + element.x * SCALE, yco + element.y * SCALE, 
 					scaledShape,
-					(i++ * 0x200020), 1.0
+					0, 1.0
 				);
 				const rect = poly.getBounds();
 				if (rect.bottom < 0 || rect.top > SCREENH || rect.right < 0 || rect.left > SCREENW) continue;
-
+		
 				poly.isFilled = false;
 				poly.isStroked = true;
 				poly.setStrokeStyle(3.0, 0xA0A0A0, 1.0);
@@ -42,19 +71,7 @@ export default class extends Phaser.Scene {
 				this.add.existing(poly);
 			}
 		}
-
-		let xco = 0;
-		let yco = 0;
-		while (yco < SCREENH) {
-			let secondX = xco;
-			let secondY = yco;
-			while (secondX < SCREENW) {
-				createPrimitiveUnit(secondX, secondY);
-				secondX += offset[2] * SCALE;
-			}
-			yco += offset[1] * SCALE;
-		}
-
+	
 	}
 
 	create () {
@@ -72,8 +89,9 @@ export default class extends Phaser.Scene {
 		// 	fill: '#7744ff'
 		// });
 
-		const t = getHexagonalTesselation();
-		this.renderTesselation(t);
+		const tesselation = getHexagonalTesselation();
+		this.grid = initGrid(tesselation);
+		this.renderPolygons(this.grid, tesselation);
 
 	}
 }
