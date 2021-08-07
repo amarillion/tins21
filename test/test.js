4ae0f90df6882338ab5d6d2ebb80f73ece126fbe
Martijn van Iersel <mvaniersel@gmail.com>
2021-08-07 21:46:33 +0200

    Set up automated tests

---
+++ test/test.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+
+import { applyToPoints, rotate, scale, transform, translate } from "transformation-matrix";
+import { TESSELATIONS } from "../src/tesselate.js"
+
+// import { SCALE } from "../src/config.js";
+const SCALE = 64;
+
+test('matrix tranformation', () => {
+	
+	const { points } = TESSELATIONS.DIAMOND;
+
+	const xco = 200;
+	const yco = 200;
+	
+	const element = {
+		x: 0,
+		y: 0,
+		rotation: Math.PI * 2 / 3 // 120 degrees
+	}
+	
+	const matrix = transform(
+		translate(xco, yco),
+		scale(SCALE, SCALE),
+		translate(element.x, element.y),
+		rotate(element.rotation),
+	);
+	
+	const newPoints = applyToPoints(matrix, points);
+	console.log(points, newPoints);
+	expect(newPoints[0]).toEqual({ x: 200, y: 200 });
+});
